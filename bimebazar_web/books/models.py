57464a0d43b43ebcc9a970692051e1087babb0d2
SirSaleh <animatorsaleh@gmail.com>
2024-08-03 00:17:37 +0330

    Add model for Books

--- bimebazar_web/books/models.py
+++ bimebazar_web/books/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Title")
+    description = models.TextField(verbose_name="Description")
+    author = models.CharField(max_length=200, verbose_name="Author")
+    genre = models.CharField(max_length=200, verbose_name="Genre")
+    published_date = models.DateField(default=timezone.now,
+                                    verbose_name="Published Date")
+
+
+    class Meta:
+        verbose_name="Book"
+        verbose_name_plural="Books"
