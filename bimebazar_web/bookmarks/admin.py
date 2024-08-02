8d45b97c302434170ec3eecfb39e653fd8549011
SirSaleh <animatorsaleh@gmail.com>
2024-08-03 01:13:58 +0330

    Add django Admin for Bookmark table

--- bimebazar_web/bookmarks/admin.py
+++ bimebazar_web/bookmarks/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Bookmark
 
-# Register your models here.
+
+class BookmarkAdmin(admin.ModelAdmin):
+    list_display = ('book', 'user')
+
+
+admin.site.register(Bookmark, BookmarkAdmin)
