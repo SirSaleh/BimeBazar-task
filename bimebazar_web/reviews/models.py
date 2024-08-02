7c0e54a4e242f571c3187a71e1f42d33711b8405
SirSaleh <animatorsaleh@gmail.com>
2024-08-03 00:58:54 +0330

    Add Book Review model schema

--- bimebazar_web/reviews/models.py
+++ bimebazar_web/reviews/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class BookReview(models.Model):
+    book = models.ForeignKey('books.Book', on_delete=models.CASCADE, related_name='book_reviews')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_reviews')
+    rating = models.PositiveSmallIntegerField(null=True, blank=True, verbose_name="Rating") 
+    review_text = models.TextField(null=True, blank=True, verbose_name="Review Text") 
+    created_at = models.DateTimeField(default=timezone.now, verbose_name="Created At")
+
+    class Meta:
+        verbose_name = "Book Review"
+        verbose_name_plural = "Book Reviews"
+        unique_together = ('book', 'user')
+
+    def __str__(self):
+        return f"Review by {self.user} on {self.book}"
