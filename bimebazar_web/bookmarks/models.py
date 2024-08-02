41edbf676996b7a0995031110a13f2cde2f56dc0
SirSaleh <animatorsaleh@gmail.com>
2024-08-03 01:13:43 +0330

    Add Bookmarark Model Schema

--- bimebazar_web/bookmarks/models.py
+++ bimebazar_web/bookmarks/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
+from django.utils import timezone
 from django.db import models
 
-# Create your models here.
+class Bookmark(models.Model):
+    book = models.ForeignKey("books.Book", on_delete=models.CASCADE, related_name='bookmarks')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='bookmarks')
+    created_at = models.DateTimeField(default=timezone.now, verbose_name="Created At")
+
+    class Meta:
+        verbose_name = "Bookmark"
+        verbose_name_plural = "Bookmarks"
+        unique_together = ('book', 'user')
+
+    def __str__(self):
+        return f"Bookmark by {self.user} on {self.book}"
