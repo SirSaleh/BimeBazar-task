820f3f73dc1a22892b6066d6a691bd735724ea47
SirSaleh <animatorsaleh@gmail.com>
2024-08-03 22:47:32 +0330

    Config swagger docs for the DRF

--- bimebazar_web/bimebazar_web/urls.py
+++ bimebazar_web/bimebazar_web/urls.py
@@ -1,7 +1,26 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="BimeBazar API",
+      default_version='v1',
+      description="This is Swagger docs for BimeBazar API",
+    #   terms_of_service="http://SirSaleh.github.io/",
+      contact=openapi.Contact(email="animatorsaleh@gmail.com"),
+    #   license=openapi.License(name="MIT"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('admin/', admin.site.urls),
     path('api/', include(('api.urls', 'api'), namespace='api')),
 ]
