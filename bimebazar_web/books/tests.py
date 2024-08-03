e94edf16253986b5ecf682750bbc633877216e3e
SirSaleh <animatorsaleh@gmail.com>
2024-08-03 13:59:32 +0330

    Add unit test for Book Model

--- bimebazar_web/books/tests.py
+++ bimebazar_web/books/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from django.utils import timezone
+from .models import Book
 
-# Create your tests here.
+class BookModelTest(TestCase):
+
+    def setUp(self):
+        self.book = Book.objects.create(
+            title="Sali Book",
+            description="This is a test book description.",
+            author="Sali",
+            genre="Dramatic",
+            published_date=timezone.now().date()
+        )
+
+    def test_book_creation(self):
+        """Test that a Book instance is created correctly."""
+        self.assertEqual(self.book.title, "Sali Book")
+        self.assertEqual(self.book.description, "This is a test book description.")
+        self.assertEqual(self.book.author, "Sali")
+        self.assertEqual(self.book.genre, "Dramatic")
+        self.assertIsNotNone(self.book.published_date)
+
+    def test_book_str(self):
+        """Test the string representation of the Book model."""
+        expected_str = f"{self.book.title} by {self.book.author} ({self.book.published_date.year})"
+        self.assertEqual(str(self.book), expected_str)
