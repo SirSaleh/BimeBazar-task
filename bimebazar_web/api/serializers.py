357c6d3f6146c930e7883762af97a88327f14322
SirSaleh <animatorsaleh@gmail.com>
2024-08-04 03:13:42 +0330

    Add serializer for the book

--- bimebazar_web/api/serializers.py
+++ bimebazar_web/api/serializers.py
@@ -1,6 +1,6 @@
-from django.contrib.auth.models import User
-from django.contrib.auth import authenticate
 from rest_framework import serializers
+from books.models import Book
+from bookmarks.models import Bookmark
 
 
 class RegisterUserSerializer(serializers.Serializer):
@@ -8,3 +8,30 @@ class RegisterUserSerializer(serializers.Serializer):
     email = serializers.EmailField()
     password = serializers.CharField()
     password2 = serializers.CharField()
+
+
+class BookDetailSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Book
+        fields = ['id']
+
+class BookListSerializer(serializers.ModelSerializer):
+    bookmarks_count = serializers.SerializerMethodField()
+    is_bookmarked = serializers.SerializerMethodField()
+
+    # todo: add detail url after implementing it
+    # detail_url = serializers.HyperlinkedIdentityField(view_name='book-detail', format='html')
+
+    class Meta:
+        model = Book
+        fields = ['id', 'title', 'bookmarks_count', 'is_bookmarked']
+
+    def get_bookmarks_count(self, obj):
+        return obj.bookmarks.count()
+
+    def get_is_bookmarked(self, obj):
+        request = self.context.get('request')
+        if request and request.user.is_authenticated:
+            return Bookmark.objects.filter(book=obj, user=request.user).exists()
+        return False
